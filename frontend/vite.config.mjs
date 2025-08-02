16afd6ca27af0117b6c30f0068d7f54b5c629f8b
kanishk-singh19 <23bcs046@nith.ac.in>
2025-08-03 03:52:29 +0530

    yossh

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -3,10 +3,9 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tagger from "@dhiwise/component-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
+  base: '/', // ✅ important for SPA deployment
+
   build: {
     outDir: "dist",
     chunkSizeWarningLimit: 2000,
